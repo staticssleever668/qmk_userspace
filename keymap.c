ab600ea2e6be28b075a6a151a53f7879e9a6ad6a
Yaroslav Chvanov <yaroslav.chvanov@gmail.com>
2023-11-22 23:30:11 +0300

    ss668-jorne: update keymap
    - Use asdfjkl; for modifiers, like in miryoku.
    - Drop some unused key bindings, especially RGB and num-pad keys.
    - Use hjkl for arrow keys, vi-mode. :)

--- keymap.c
+++ keymap.c
@@ -33,14 +33,23 @@ enum MY_LAYER___________ {
 #define GAY_LYR TO(MY_LAYER_GAYMING)
 #define BSE_LYR TO(MY_LAYER_BASE)
 
+#define LALT_L MT(MOD_LALT, KC_L)
+#define LALT_S MT(MOD_LALT, KC_S)
+#define LCTL_D MT(MOD_LCTL, KC_D)
+#define LCTL_K MT(MOD_LCTL, KC_K)
+#define LGUI_A MT(MOD_LGUI, KC_A)
+#define LG_CLN MT(MOD_LGUI, KC_SCLN)
+#define LSFT_F MT(MOD_LSFT, KC_F)
+#define LSFT_J MT(MOD_LSFT, KC_J)
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [MY_LAYER_BASE] = LAYOUT(
-  KC_LGUI, KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, RBR_RGU,
-           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RCTL_T(KC_QUOT),
-           KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, BSL_RAL,
-                                      TAB_RSE, SPC_RSH, ESC_LWR,      ENT_LWR, BSP_LSH, DEL_RSE
+  _______, KC_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,
+           _______, LGUI_A,  LALT_S,  LCTL_D,  LSFT_F,  KC_G,         KC_H,    LSFT_J,  LCTL_K,  LALT_L,  LG_CLN,  KC_QUOT,
+           _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, BSL_RAL,
+                                      TAB_RSE, KC_SPC,  ESC_LWR,      ENT_LWR, KC_BSPC, DEL_RSE
 ),
 
 [MY_LAYER_LOWER] = LAYOUT(
@@ -51,17 +60,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 [MY_LAYER_RAISE] = LAYOUT(
-  _______, KC_NUM,  KC_PSLS, KC_P7,   KC_P8,   KC_P9,   KC_PMNS,      KC_VOLU, KC_HOME, KC_PSCR, KC_PGUP, KC_SCRL, KC_CAPS, _______,
-           EQL_LCT, KC_PAST, KC_P4,   KC_P5,   KC_P6,   KC_PPLS,      KC_MUTE, KC_LEFT, KC_UP,   KC_RGHT, KC_INS,  APP_RCT,
-           _______, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PCMM,      KC_VOLD, KC_END,  KC_DOWN, KC_PGDN, KC_PAUS, _______,
+  _______, _______, _______, _______, _______, _______, _______,      _______, KC_PSCR, _______, _______, _______, _______, _______,
+           _______, _______, _______, _______, _______, _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+           _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
                                       _______, _______, _______,      _______, _______, _______
 ),
 
 [MY_LAYER_ADJUST] = LAYOUT(
-  QK_BOOT, _______,  AS_UP,   AS_TOGG, AS_DOWN, _______, _______,     GAY_LYR, _______, AS_DOWN, AS_TOGG, AS_UP,   _______,  QK_BOOT,
-           RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,      _______, _______, RGB_VAI, RGB_SAI, RGB_HUI, RGB_TOG,
-           RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, _______, _______,      _______, _______, RGB_VAD, RGB_SAD, RGB_HUD, RGB_MOD,
-                                      _______, SH_TOGG, _______,      _______, SH_TOGG, _______
+  QK_BOOT, _______, _______, _______, _______, _______, _______,      GAY_LYR, _______, _______, _______, _______, _______,  QK_BOOT,
+           _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
+           _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______,
+                                      _______, _______, _______,      _______, _______, _______
 ),
 
 [MY_LAYER_GAYMING] = LAYOUT(
